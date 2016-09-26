6820eb90b8bc018968984bc92080495b43129fcc
piipoo12 <phamminhtue16@gmail.com>
2016-09-26 09:23:08 +0700

    aye

---
+++ ConsoleApplication1/ConsoleApplication1/List.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class List<T>
+
+    {
+        private Element<T> head;
+
+        internal Element<T> Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+
+        public List()
+        {}
+            public void addfirst(Element<T> e){
+                e.Next = head;
+                this.head = e;
+            }
+        }
+    }
